8ae710a92604180a8627a598b81c1838d429bd12
papa0four <papa.o.deuce@gmail.com>
2022-06-29 18:03:38 -0400

    adding more functionality and making it take cmdline arg

--- automation/phone_number.py
+++ automation/phone_number.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+
+import argparse
+
+def get_arg() -> str:
+    parser = argparse.ArgumentParser(description='Phone Number Validator')
+    parser.add_argument('phone_number', type=str, nargs=1,
+                        help='takes a 10, 12, or 14 digit American phone number as a string:\n' \
+                             'examples: requires no quotes - 1234567890, 123-456-7890\n' \
+                             'requires quotes - "(123) 456 7890", "(123)-456-7890"')
+    pnum = parser.parse_args()
+    return pnum.phone_number[0]
+
 def is_phone_number(text: str) -> bool:
     if (len(text) < 10 or len(text) > 14):
         return False
@@ -25,14 +37,29 @@ def is_phone_number(text: str) -> bool:
 
         else:
             return False
+
+    elif len(text) == 13:
+        if (text[0] == '(' and
+            text[4] == ')' and
+            text[8] == '-'):
+            for i in range(1, 4):
+                if not text[i].isdecimal():
+                    return False
+            for i in range(5, 8):
+                if not text[i].isdecimal():
+                    return False
+            for i in range(9, 13):
+                if not text[i].isdecimal():
+                    return False
+            return True
+        else:
+            return False
     
     elif len(text) == 14:
         if (text[0] == '(' and
             text[4] == ')' and
-            text[5] == ' ' or
-            text[5] == '-' and
-            text[9] == ' ' or
-            text[9] == '-'):
+            text[5] == ' ' and
+            text[9] == ' '):
             for i in range(1, 4):
                 if not text[i].isdecimal():
                     return False
@@ -47,59 +74,67 @@ def is_phone_number(text: str) -> bool:
         return False
 
 if __name__ == '__main__':
-    print('Testing is_phone_number function:\n')
-
-    print('Testing False len < 10')
-    if is_phone_number('1234567') is False:
-        print('success')
-    else:
-        print('failed on too short test')
-
-    print('Testing False len > 14')
-    if is_phone_number('123456789012345') is False:
-        print('success')
-    else:
-        print('failed on too long test')
-
-    print('Testing True 10 digit: 1234567890')
-    if is_phone_number('1234567890'):
-        print('success')
-    else:
-        print('failed on 10 digit test')
-
-    print('Testing True 12 digit: 123-456-7890')
-    if is_phone_number('123-456-7890'):
-        print('success')
-    else:
-        print('failed on 12 digit test')
-
-    print('Testing True on 14 digit spaces: (123) 456 7890')
-    if is_phone_number('(123) 456 7890'):
-        print('success')
-    else:
-        print('failed on 14 digit spaces test')
-
-    print('Testing True on 14 digit tac: (123)-456-7890')
-    if is_phone_number('(123)-456-7890'):
-        print('success')
-    else:
-        print('failed on 14 digit test tac')
-
-    print('Testing False 10 digit: 123apple45')
-    if is_phone_number('123apple45') is False:
-        print('success')
-    else:
-        print('failed on 10 digit False test')
-
-    print('Testing False 12 digit: 123456789012')
-    if is_phone_number('123456789012') is False:
-        print('success')
-    else:
-        print('failed on 12 digit False')
-
-    print('Testing False 14 digit: (sam) 123 4567')
-    if is_phone_number('(sam) 123 4567') is False:
-        print('success')
-    else:
-        print('failed on 14 digit false')
+#    print('Testing is_phone_number function:\n')
+#
+#    print('Testing False len < 10')
+#    if is_phone_number('1234567') is False:
+#        print('success')
+#    else:
+#        print('failed on too short test')
+#
+#    print('Testing False len > 14')
+#    if is_phone_number('123456789012345') is False:
+#        print('success')
+#    else:
+#        print('failed on too long test')
+#
+#    print('Testing True 10 digit: 1234567890')
+#    if is_phone_number('1234567890'):
+#        print('success')
+#    else:
+#        print('failed on 10 digit test')
+#
+#    print('Testing True 12 digit: 123-456-7890')
+#    if is_phone_number('123-456-7890'):
+#        print('success')
+#    else:
+#        print('failed on 12 digit test')
+#
+#    print('Testing True on 14 digit spaces: (123) 456 7890')
+#    if is_phone_number('(123) 456 7890'):
+#        print('success')
+#    else:
+#        print('failed on 14 digit spaces test')
+#
+#    print('Testing True on 14 digit tac: (123)-456-7890')
+#    if is_phone_number('(123)-456-7890'):
+#        print('success')
+#    else:
+#        print('failed on 14 digit test tac')
+#
+#    print('Testing False 10 digit: 123apple45')
+#    if is_phone_number('123apple45') is False:
+#        print('success')
+#    else:
+#        print('failed on 10 digit False test')
+#
+#    print('Testing False 12 digit: 123456789012')
+#    if is_phone_number('123456789012') is False:
+#        print('success')
+#    else:
+#        print('failed on 12 digit False')
+#
+#    print('Testing False 14 digit: (sam) 123 4567')
+#    if is_phone_number('(sam) 123 4567') is False:
+#        print('success')
+#    else:
+#        print('failed on 14 digit false')
+    pnum = get_arg()
+    if is_phone_number(pnum):
+        print(f'{pnum} is valid')
+        exit(0)      
+    
+    print('Number passed is not a valid phone number')
+    print('type ./phone_number.py -h for examples')
+    exit(-1)
 
