467f0afa7200706cd5da9806a08ac0293f7697c5
papa0four <papa.o.deuce@gmail.com>
2022-06-29 18:35:35 -0400

    removed most logic and added regex for parsing

--- automation/phone_number.py
+++ automation/phone_number.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
+from argparse import RawTextHelpFormatter
 
 def get_arg() -> str:
-    parser = argparse.ArgumentParser(description='Phone Number Validator')
-    parser.add_argument('phone_number', type=str, nargs=1,
+    parser = argparse.ArgumentParser(description='Phone Number Validator',
+                                    formatter_class=RawTextHelpFormatter)
+    parser.add_argument('phone_number', type=str, action='store', nargs='+',
                         help='takes a 10, 12, or 14 digit American phone number as a string:\n' \
-                             'examples: requires no quotes - 1234567890, 123-456-7890\n' \
-                             'requires quotes - "(123) 456 7890", "(123)-456-7890"')
+                             'examples:\n<requires no quotes> - 1234567890, 123-456-7890\n' \
+                             '<requires quotes> - "(123) 456 7890", "(123)-456-7890"')
     pnum = parser.parse_args()
     return pnum.phone_number[0]
 
@@ -15,120 +18,15 @@ def is_phone_number(text: str) -> bool:
     if (len(text) < 10 or len(text) > 14):
         return False
 
-    if len(text) == 10:
-        for i in range(0, len(text)):
-            if not text[i].isdecimal():
-                return False
-        return True
-    
-    elif len(text) == 12:
-        if (text[3] == '-' and
-            text[7] == '-'):
-            for i in range(0, 3):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(4, 7):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(8, 12):
-                if not text[i].isdecimal():
-                    return False
-            return True
-
-        else:
-            return False
-
-    elif len(text) == 13:
-        if (text[0] == '(' and
-            text[4] == ')' and
-            text[8] == '-'):
-            for i in range(1, 4):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(5, 8):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(9, 13):
-                if not text[i].isdecimal():
-                    return False
-            return True
-        else:
-            return False
-    
-    elif len(text) == 14:
-        if (text[0] == '(' and
-            text[4] == ')' and
-            text[5] == ' ' and
-            text[9] == ' '):
-            for i in range(1, 4):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(6, 9):
-                if not text[i].isdecimal():
-                    return False
-            for i in range(10,14):
-                if not text[i].isdecimal():
-                    return False
-            return True            
-    else:
+    regex = "(\d{3}[-\.\s]??\d{3}[-\.\s]??" \
+            "\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??" \
+            "\d{4}|\d{3}[-\.\s]??\d{4})"
+    match = re.search(regex, text)
+    if match is None:
         return False
+    return True
 
 if __name__ == '__main__':
-#    print('Testing is_phone_number function:\n')
-#
-#    print('Testing False len < 10')
-#    if is_phone_number('1234567') is False:
-#        print('success')
-#    else:
-#        print('failed on too short test')
-#
-#    print('Testing False len > 14')
-#    if is_phone_number('123456789012345') is False:
-#        print('success')
-#    else:
-#        print('failed on too long test')
-#
-#    print('Testing True 10 digit: 1234567890')
-#    if is_phone_number('1234567890'):
-#        print('success')
-#    else:
-#        print('failed on 10 digit test')
-#
-#    print('Testing True 12 digit: 123-456-7890')
-#    if is_phone_number('123-456-7890'):
-#        print('success')
-#    else:
-#        print('failed on 12 digit test')
-#
-#    print('Testing True on 14 digit spaces: (123) 456 7890')
-#    if is_phone_number('(123) 456 7890'):
-#        print('success')
-#    else:
-#        print('failed on 14 digit spaces test')
-#
-#    print('Testing True on 14 digit tac: (123)-456-7890')
-#    if is_phone_number('(123)-456-7890'):
-#        print('success')
-#    else:
-#        print('failed on 14 digit test tac')
-#
-#    print('Testing False 10 digit: 123apple45')
-#    if is_phone_number('123apple45') is False:
-#        print('success')
-#    else:
-#        print('failed on 10 digit False test')
-#
-#    print('Testing False 12 digit: 123456789012')
-#    if is_phone_number('123456789012') is False:
-#        print('success')
-#    else:
-#        print('failed on 12 digit False')
-#
-#    print('Testing False 14 digit: (sam) 123 4567')
-#    if is_phone_number('(sam) 123 4567') is False:
-#        print('success')
-#    else:
-#        print('failed on 14 digit false')
     pnum = get_arg()
     if is_phone_number(pnum):
         print(f'{pnum} is valid')
