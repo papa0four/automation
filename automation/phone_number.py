ea79142341fdcf9ba532fa32afd8b76a0669111f
papa0four <papa.o.deuce@gmail.com>
2022-06-30 14:26:09 -0400

    updated help message and added comment explaining regex

--- automation/phone_number.py
+++ automation/phone_number.py
@@ -8,9 +8,10 @@ def get_arg() -> str:
     parser = argparse.ArgumentParser(description='Phone Number Validator',
                                     formatter_class=RawTextHelpFormatter)
     parser.add_argument('phone_number', type=str, action='store', nargs='+',
-                        help='takes a 10, 12, or 14 digit American phone number as a string:\n' \
+                        help='takes a 10, 12, 13, or 14 digit American phone number as a string:\n' \
                              'examples:\n<requires no quotes> - 1234567890, 123-456-7890\n' \
-                             '<requires quotes> - "(123) 456 7890", "(123)-456-7890"')
+                             '<requires quotes> - "123 456 7890", "(123)456-7890, ' \
+                             '"(123)456 7890",\n\t\t    "(123) 456 7890", "(123)-456-7890"')
     pnum = parser.parse_args()
     return pnum.phone_number[0]
 
@@ -18,11 +19,42 @@ def is_phone_number(text: str) -> bool:
     if (len(text) < 10 or len(text) > 14):
         return False
 
-    regex = "(\d{3}[-\.\s]??\d{3}[-\.\s]??" \
-            "\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??" \
-            "\d{4}|\d{3}[-\.\s]??\d{4})"
+
+    """
+    REGEX EXPLAINED:
+        1st pairing of \d{3}[-\.\s]?? - handles first group of 3 digits followed by
+        '-', '.', or ' '
+
+        2nd pairing of \d{3}[-\.\s]?? - handles second group of 3 digits followed by
+        '-', '.', or ' '
+
+        1st pairing of \d{4} - handles last group of 4 digits
+
+        | - bitwise or operator for alternative pairings 
+
+        \(\d{3}\) - handles 3 digit area codes enclosed in ()
+
+        \s* - handles all ' ' characters (special character)
+
+        3rd pairing of \d{3}[-\.\s]?? - handles group of three digits that follow ' '
+        and then is followed by '-', '.', or ' '
+
+        2nd pairing of \d{4} - handles last group of 4 digits
+
+        | - bitwise or operator for alternative pairings
+
+        4th pairing of \d{3}[-\.\s]?? - handles second group of 3 digits that follow ' '
+        and then is followed by '-', '.', or ' '
+
+        3rd pairing of \d{4} - handles last group of 4 digits that follow '-', '.', or ' '
+    """
+    regex = "(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|" \
+            "\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|" \
+            "\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4})" 
+
     match = re.search(regex, text)
     if match is None:
+        print(f"match.group(): {match.group}\ttype: {type(match.group())}")
         return False
     return True
 
